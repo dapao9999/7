24b705959c02dbfa23d30c0a54c036d7c20cfdaf
dcdc8888 <rph322@gmail.com>
2017-04-25 13:11:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,6 +18,7 @@ export LKL_HIJACK_NET_IFPARAMS=tap0
 export LKL_HIJACK_NET_IP=10.0.0.2
 export LKL_HIJACK_NET_NETMASK_LEN=24
 export LKL_HIJACK_NET_GATEWAY=10.0.0.1
+export LKL_HIJACK_OFFLOAD=0x883
 export LKL_HIJACK_DEBUG=0
 
 haproxy -f /root/lkl/haproxy.cfg
