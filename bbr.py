91066d73ba46c4263e52724ef743b82f7ac13a7e
dcdc8888 <rph322@gmail.com>
2020-06-07 22:37:44 +0800

    Update bbr.py

--- bbr.py
+++ bbr.py
@@ -1,7 +1,4 @@
-import subprocess
-
-with open('/etc/sysctl.conf','a') as f:
-    f.write('net.core.default_qdisc=fq\nnet.ipv4.tcp_congestion_control=bbr\n')
-
-subprocess.Popen(['sysctl','-p'])
-subprocess.Popen(['cat','/etc/sysctl.conf'])
+import os
+with open('/etc/sysctl.conf', 'a') as f:
+	f.write('net.core.default_qdisc=fq\nnet.ipv4.tcp_congestion_control=bbr\n')
+os.system('sysctl -p')
